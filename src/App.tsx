e5a7cc3e31b24195ad83e14c19fe7034242012ae
Daniel <d.groen@codefield.nl>
2024-06-12 20:00:53 +0200

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { TradingviewComponent } from './components/TradingviewComponent';
+import { SidebarAnonymous } from './components/SidebarAnonymous';
+import { Box } from '@mui/material';
 
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="overflow-x-hidden">
+      <div className="bg-gradient" />
+      <Box sx={{ display: 'flex' }}>
+        {/* <TradingviewComponent /> */}
+        <div
+          style={{
+            width: '400px',
+            padding: 16,
+            display: 'flex',
+            height: '100vh',
+            flexDirection: 'column',
+          }}
+        >
+          <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+            <SidebarAnonymous />
+          </Box>
+        </div>
+      </Box>
+    </div>
+  );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import './index.css'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      paper: '#0e1526',
+      default: '#0e1526',
+    },
+    primary: {
+      main: '#00b0ff',
+    },
+  },
+});
+
+(window as any).global = window;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
