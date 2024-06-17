dfbd3fe012aaa0421e0873d333fe7957874541c4
Daniel <d.groen@codefield.nl>
2024-06-17 16:04:34 +0200

    up

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Specify the output directory
     rollupOptions: {
-      external: ['ccxt', 'https-proxy-agent'],
+      external: ['ccxt'],
     },
   },
   server: {
