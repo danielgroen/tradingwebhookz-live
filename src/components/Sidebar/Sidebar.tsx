007d81457b9802922dfbe76c7a5d1d9bb1e703da
Daniel <d.groen@codefield.nl>
2024-07-25 18:02:12 +0200

    up

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Link } from '@mui/material';
 import { LoginForm, Header, SettingsForm, OrderForm } from '@components/index';
 import { GlobalState } from '@states/index';
 
@@ -14,10 +14,11 @@ export const Sidebar = () => {
         height: '100vh',
         overflow: 'auto',
         flexDirection: 'column',
-        boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
       }}
     >
-      <Box sx={{ flexGrow: 1, display: 'flex', p: 2, flexDirection: 'column' }}>
+      <Box
+        sx={{ flexGrow: 1, display: 'flex', p: 2, boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', flexDirection: 'column' }}
+      >
         <div className="flex flex-col h-[100%]">
           {showSidebar && (
             <>
@@ -27,6 +28,17 @@ export const Sidebar = () => {
           )}
         </div>
       </Box>
+      <Link
+        textAlign="center"
+        fontSize={10}
+        height={'39px'}
+        sx={{ p: '11px', borderTop: '1px solid #2a2e39', background: '#131722' }}
+        href="https://www.tradingview.com/"
+        rel="noopener nofollow"
+        target="_blank"
+      >
+        Track all markets on TradingView
+      </Link>
     </div>
   );
 };
