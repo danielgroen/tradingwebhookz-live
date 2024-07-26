337de4622dfc0f597c94a944b920be40b387787e
Daniel <d.groen@codefield.nl>
2024-07-26 16:00:12 +0200

    up

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Box, Link } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 import { LoginForm, Header, SettingsForm, OrderForm } from '@components/index';
 import { GlobalState } from '@states/index';
 
@@ -28,17 +28,21 @@ export const Sidebar = () => {
           )}
         </div>
       </Box>
-      <Link
+      <Typography
         textAlign="center"
         fontSize={10}
         height={'39px'}
-        sx={{ p: '11px', borderTop: '1px solid #2a2e39', background: '#131722' }}
-        href="https://www.tradingview.com/"
-        rel="noopener nofollow"
-        target="_blank"
+        sx={{ p: '5px', borderTop: '1px solid #2a2e39', background: '#131722', opacity: 0.8 }}
       >
-        Track all markets on TradingView
-      </Link>
+        <Link fontSize={10} href="https://www.tradingview.com" rel="noopener nofollow" target="_blank">
+          Tradingview
+        </Link>{' '}
+        is a platform for traders and high-performance market data to help track coins like{' '}
+        <Link fontSize={10} href="https://www.tradingview.com/symbols/BTCUSD/" rel="noopener nofollow" target="_blank">
+          BTCUSDT
+        </Link>{' '}
+        prices on charts.
+      </Typography>
     </div>
   );
 };
